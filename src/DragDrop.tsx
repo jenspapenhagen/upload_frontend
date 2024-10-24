19718eddf51e8be8c46a30cb2e2258cf43f56ea2
jenspapenhagen <jens.papenhagen@web.de>
2024-10-24 21:51:47 +0200

    cleanup

--- src/DragDrop.tsx
+++ src/DragDrop.tsx
@@ -9,6 +9,12 @@ function DragDrop() {
   const handleChange = (file: File) => {
     setFile(file);
   };
+  
+  useEffect(()=> {    
+    if (file) {
+       upload(file);
+    }   
+  },[file]);
 
   const upload = (file: File) => {
     const formData = new FormData()
@@ -25,22 +31,11 @@ function DragDrop() {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0',
       },
       body: formData
-    }).then(
-      response => response.json() // if the response is a JSON object
-    ).then(
-      success => console.log(success) // Handle the success response object
-    ).catch(
-      error => console.log(error) // Handle the error response object
+    }).catch(
+      error => console.log(error)
     );
   };
   
-
-  useEffect(()=> {    
-    if (file) {
-       upload(file);
-    }   
-  },[file]);
-
   return (
     <FileUploader handleChange={handleChange} name="fileupload" types={fileTypes} />
   );
