9ad2fbe54fc6640279b459f0d063cd153fe67140
jenspapenhagen <jens.papenhagen@web.de>
2024-12-17 21:21:30 +0100

    adding cleaner fetch method

--- src/DragDrop.tsx
+++ src/DragDrop.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import { FileUploader } from "react-drag-drop-files";
 
 const fileTypes = ["JPG", "PDF"];
@@ -8,20 +8,25 @@ function DragDrop() {
 
   const handleChange = (file: File) => {
     setFile(file);
+    if (file) {
+      upload();
+    }
   };
 
 
   //get URL from env
   const url: string = process.env.API_URL ?? 'http://localhost:8080/files.html';
 
-  const upload = useCallback(() => {
+  const upload = useCallback(async () => {
     const formData = new FormData()
-    if (!file) {
-      throw new Error("file is not set");
-    }
+    if(!file) {
+      console.log("file is not set");
+      return;
+    } 
+    
     formData.append("fileupload", file, file.name);
 
-    fetch(url, {
+    const response = await fetch(url, {
       method: 'POST',
       mode: 'no-cors',
       cache: 'no-cache',
@@ -32,19 +37,27 @@ function DragDrop() {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0',
       },
       body: formData
-    }).catch(
-      error => console.log(error)
-    );
+    });
+
+    const { data, error } = await response.json();
+
+    if (response.ok) {
+      const payload = data?.payload;
+      if (payload) {
+        // validate payload       
+        return payload
+      } else {
+        return Promise.reject(new Error(`No payload in the response`))
+      }
+    } else {
+      // handle the errors
+      return Promise.reject(error)
+    }
+    
   }, [file, url]);
 
-  useEffect(() => {
-    if (file) {
-      upload();
-    }
-  }, [file, upload]);
-
   return (
-    <FileUploader handleChange={handleChange} name="fileupload" types={fileTypes} />
+    <FileUploader handleChange={handleChange} name="fileupload" types={fileTypes} data-testid="fileuploader" />
   );
 }
 
